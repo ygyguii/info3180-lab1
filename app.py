f1b31293f77329cd0b1096af547dd4177be9518d
ygyguii2 <whatswrong4@gmail.com>
2026-02-16 22:39:57 -0500

    created my first template file

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-
 def home():
 	return 'My home page'
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
 
 @app.errorhandler(404)
 def page_not_found(error):
